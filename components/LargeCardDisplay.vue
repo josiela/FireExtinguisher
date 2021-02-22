b3688b59aba0e97b06bcbd69ad22d1fd9c629852
Josie <joseffa.steuernagel@haw-hamburg.de>
2021-02-22 16:46:32 +0100

    Commit setup

---
+++ components/LargeCardDisplay.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <h4 class="header">
+            {{ cardSection.title }}
+        </h4>
+        <p class="snippet">
+            {{ cardSection.snippet }}
+        </p>
+        <div class="cards-container">
+            <!-- in here we're going to display the largCard-Components. Dafür iterieren wir nun durch das cards-Array in der data.js -->
+            <LargeCard v-for="card in cardSection.cards" :key="card.id" :card="card"/>
+            <!-- :card is a prop that we're übergeben. This prop we define in the props-section in LargeCard.vue-->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['cardSection']
+}
+</script>
+
+<style scoped>
+.container {
+    margin-top: 2rem;
+}
+    .header {
+        font-weight: 700;
+        font-size: 1.5rem;
+    }
+    .snippet {
+        color: grey;
+        margin-bottom: 1.5rem;
+    }
+    .cards-container {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Nav/>
+    <div class="container">
+      <!-- we pull each component in: -->
+      <Hero/>
+      <!-- So instead of calling them in manually with
+      <LargeCardDisplay/>
+      we can call it in with a v-for-loop -->
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo" :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections" :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+//why largeCardSections: in data.js the const in which the data is stored is calles largeCardSections so we're calling that const
+  import { largeCardSections, smallCardSections } from '@/assets/data.js';
+  export default {
+    data() {
+      return {
+        //associate largeCardInfo with the data-const
+        largeCardInfo: largeCardSections,
+        smallCardSections
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
